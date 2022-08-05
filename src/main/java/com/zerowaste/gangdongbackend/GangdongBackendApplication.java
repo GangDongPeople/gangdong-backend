eed337ec9b2d382c306c5562ec79af62c5444c40
hyukjun <hyukjuuun@gmail.com>
2022-08-05 22:40:22 +0900

    create project

---
+++ src/main/java/com/zerowaste/gangdongbackend/GangdongBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zerowaste.gangdongbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangdongBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GangdongBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerowaste/gangdongbackend/GangdongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerowaste.gangdongbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GangdongBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
